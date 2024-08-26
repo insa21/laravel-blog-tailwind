2d7c0f54b5dc08187ef58e956fe054d235db6590
Indra Saepudin <indrasaepudin212@gmail.com>
2024-08-26 20:58:02 +0700

    Initial commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,33 @@
+<!-- Start Generation Here -->
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+
+<body>
+    <article>
+        <h3>Judul 1</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error optio officiis in, perferendis suscipit
+            voluptate accusamus voluptates minus adipisci, libero nesciunt, enim voluptas necessitatibus unde
+            consequatur nobis cumque vero quod.
+        </p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error optio officiis in, perferendis suscipit
+            voluptate accusamus voluptates minus adipisci, libero nesciunt, enim voluptas necessitatibus unde
+            consequatur nobis cumque vero quod.
+        </p>
+    </article>
+</body>
+
+</html>
+<!-- End Generation Here -->
