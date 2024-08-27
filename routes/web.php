0f895c1275043d989da68d0d3f47c46368f4d664
Indra Saepudin <indrasaepudin212@gmail.com>
2024-08-27 21:04:42 +0700

    slug and detail data in View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,8 +12,57 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Indra Saepudin',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eius autem et quaerat libero sed blanditiis
+            vel dicta facere nulla nihil sapiente laboriosam eos minus, fugit labore temporibus. Laborum, sint.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Indra Saepudin',
+            'body' => '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eius autem et quaerat libero sed blanditiis
+            vel dicta facere nulla nihil sapiente laboriosam eos minus, fugit labore temporibus. Laborum, sint. Lorem
+            ipsum dolor, sit amet consectetur adipisicing elit. Fugit enim, ea, perspiciatis aliquam minus nam omnis
+            dolore sequi eaque commodi pariatur quas. Ad, libero fuga possimus animi sunt temporibus minima.'
+        ]
+    ]]);
+});
+
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Indra Saepudin',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eius autem et quaerat libero sed blanditiis
+            vel dicta facere nulla nihil sapiente laboriosam eos minus, fugit labore temporibus. Laborum, sint.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Indra Saepudin',
+            'body' => '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eius autem et quaerat libero sed blanditiis
+            vel dicta facere nulla nihil sapiente laboriosam eos minus, fugit labore temporibus. Laborum, sint. Lorem
+            ipsum dolor, sit amet consectetur adipisicing elit. Fugit enim, ea, perspiciatis aliquam minus nam omnis
+            dolore sequi eaque commodi pariatur quas. Ad, libero fuga possimus animi sunt temporibus minima.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 
