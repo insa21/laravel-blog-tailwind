16e53ec3720fc09a7add8a9c01853894ba0e24fb
Indra Saepudin <indrasaepudin212@gmail.com>
2024-10-03 10:55:40 +0700

    model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,47 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-  public static function all()
-  {
-    return [
-      [
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Indra Saepudin',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eius autem et quaerat libero sed blanditiis
-                vel dicta facere nulla nihil sapiente laboriosam eos minus, fugit labore temporibus. Laborum, sint.'
-      ],
-      [
-        'id' => 2,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Indra Saepudin',
-        'body' => '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eius autem et quaerat libero sed blanditiis
-                vel dicta facere nulla nihil sapiente laboriosam eos minus, fugit labore temporibus. Laborum, sint. Lorem
-                ipsum dolor, sit amet consectetur adipisicing elit. Fugit enim, ea, perspiciatis aliquam minus nam omnis
-                dolore sequi eaque commodi pariatur quas. Ad, libero fuga possimus animi sunt temporibus minima.'
-      ]
-    ];
-  }
-
-  public static function find($slug): array
-  {
-    // return Arr::first(static::all(), function ($post) use ($slug) {
-    //   return $post['slug'] == $slug;
-    // });
-
-    // Versi Arrow Function
-    $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-    if (! $post) {
-      abort(404);
-    }
-
-    return $post;
-  }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 ">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="">{{ $post['author'] }}</a> | 27 Agustus 2024
+                <a href="">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y') }}
             </div>
             <p class="my-4 font-light">
                 {{ Str::limit($post['body'], 200) }}
@@ -15,7 +15,5 @@
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
     @endforeach
-
-
 
 </x-Layout>
