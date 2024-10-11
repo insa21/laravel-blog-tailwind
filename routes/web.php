0b8f096cca1cac111cb7dbd35bd4ef17f96029d6
Indra Saepudin <indrasaepudin212@gmail.com>
2024-10-12 00:52:14 +0700

    Database Relasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     // $post = Post::find($id);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+
+Route::get('/authors/{user}', function (User $user) {
+
+    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 
